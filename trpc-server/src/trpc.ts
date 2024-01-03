0a2ab192a138bab1ed65181e023e27160122c4f0
divyam gupta <divyam7091@gmail.com>
2024-01-03 23:59:44 +0530

    initial client

--- trpc-server/src/trpc.ts
+++ trpc-server/src/trpc.ts
@@ -1,9 +1,27 @@
 import { initTRPC } from "@trpc/server";
-
+import * as trpcExpress from "@trpc/server/adapters/express";
+import express from "express";
+import { userRouter } from "./routers/user";
+import { postRouter } from "./routers/post";
+import { commentRouter } from "./routers/comment";
 const t = initTRPC.create();
 export const router = t.router;
 export const publicProcedure = t.procedure;
 
-export const appRouter = router({});
+export const appRouter = router({
+  user: userRouter,
+  post: postRouter,
+  comment: commentRouter,
+});
 
 export type AppRouter = typeof appRouter;
+
+const app = express();
+app.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+  })
+);
+
+app.listen(4000, () => console.log("started"));
